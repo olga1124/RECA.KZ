68628a4bba8f679222608fc6e5fed22cd7f592b3
German Blonski <germanblonski@gmail.com>
2024-05-27 23:40:44 +0200

    include env var in the config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_SERVICE_ID: process.env.NEXT_PUBLIC_SERVICE_ID,
+        NEXT_PUBLIC_TEMPLATE_NEW_LEAD: process.env.NEXT_PUBLIC_TEMPLATE_NEW_LEAD,
+        NEXT_PUBLIC_USER_ID: process.env.NEXT_PUBLIC_USER_ID,
+        NEXT_PUBLIC_TEMPLATE_UPLOAD_CV: process.env.NEXT_PUBLIC_TEMPLATE_UPLOAD_CV
+      },
+};
 
 export default nextConfig;
