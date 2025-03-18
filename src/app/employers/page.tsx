e43956e8a17ed5fd50c1381e40ac9aaa8b8bff36
ukeem <contact@reca.kz>
2025-03-18 15:31:53 +0500

    Update page.tsx

--- src/app/employers/page.tsx
+++ src/app/employers/page.tsx
@@ -3,6 +3,8 @@ import { employersHero } from "@/constants/employers";
 import Contact from "@/sections/Contact";
 import Stages from "@/sections/Stages";
 import { stagesEmployers } from "@/constants/stage-employee"
+import Employers from "@/sections/Employers";
+import { employersServices } from "@/constants/employers";
 import EmployersAbout from '@/sections/EmployersAbout';
 
 export default function Home() {
